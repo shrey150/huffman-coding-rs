d628a0d5948d4e6dc7cbb7ce9cf1418c550ac1eb
Shrey Pandya <shrey150@yahoo.com>
2023-07-04 00:19:44 -0400

    messy huffman code generation

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ type ChildNode = Option<Box<Node>>;
 struct Node {
     left: ChildNode,
     right: ChildNode,
-    letter: char,
+    letter: Option<char>,
     count: usize,
 }
 
@@ -41,7 +41,7 @@ fn gen_huff_tree(mut letters: HashMap<char, usize>) -> Box<Node> {
     let mut heap: BinaryHeap<_> = letters
     .drain()
     .map(|(letter, count)| Box::new(Node {
-        letter,
+        letter: Some(letter),
         count,
         left: None,
         right: None
@@ -57,7 +57,7 @@ fn gen_huff_tree(mut letters: HashMap<char, usize>) -> Box<Node> {
         heap.push(Box::new(Node {
             left: Some(n1),
             right: Some(n2),
-            letter: '\0',   // indicates this doesn't have a letter
+            letter: None,
             count: sum_count,
         }));
     }
@@ -66,11 +66,40 @@ fn gen_huff_tree(mut letters: HashMap<char, usize>) -> Box<Node> {
     heap.pop().unwrap()
 }
 
-fn print_tree(root: Node, branch: &str, depth: usize) {
-    println!("{}{}( {} | {} )", "│  ".repeat(depth), branch, root.letter, root.count);
+fn print_tree(root: &Node, branch: &str, depth: usize) {
+
+    println!(
+        "{}{}( {} | {} )", "│  ".repeat(depth),
+        branch,
+        root.letter.unwrap_or(' '),
+        root.count
+    );
+
     let child_branch = if root.right.is_none() { "└──" } else { "├──" };
-    if let Some(nl) = root.left { print_tree(*nl, child_branch, depth+1) }
-    if let Some(nr) = root.right { print_tree(*nr, child_branch, depth+1) }
+    if let Some(nl) = &root.left { print_tree(&nl, child_branch, depth+1) }
+    if let Some(nr) = &root.right { print_tree(&nr, child_branch, depth+1) }
+}
+
+fn huff_encode(root: &Node) -> HashMap<char, Vec<u8>> {
+    let mut map: HashMap<char, Vec<u8>> = HashMap::new();
+    get_huff_codes(root, vec![], &mut map);
+    map
+}
+
+fn get_huff_codes(n: &Node, bits: Vec<u8>, map: &mut HashMap<char, Vec<u8>>) {
+    // invariant: a leaf node will always have Some letter and None for children
+    if n.letter.is_some() && n.left.is_none() && n.right.is_none() {
+        map.entry(n.letter.clone().unwrap()).or_insert(bits);
+    }
+    else {
+        let mut bits_l = bits.clone();
+        bits_l.push(0);
+        get_huff_codes(n.left.as_ref().unwrap(), bits_l, map);
+    
+        let mut bits_r = bits.clone();
+        bits_r.push(1);
+        get_huff_codes(n.right.as_ref().unwrap(), bits_r, map);
+    }
 }
 
 fn main() {
@@ -81,5 +110,8 @@ fn main() {
     println!("Frequency map: {:?}", letters);
 
     let huff_tree = gen_huff_tree(letters);
-    print_tree(*huff_tree, "", 0);
+    print_tree(&huff_tree, "", 0);
+
+    let map = huff_encode(&huff_tree);
+    println!("{:?}", map);
 }
